c0b682abf6ce09fdc3ede5173b05b6e9f4ed8fc3
Freddie Haddad <freddie.haddad@outlook.com>
2022-07-09 17:58:13 -0700

    Initial CleanActionBars addon created

---
+++ CleanActionBars.lua
@@ -0,0 +1,8 @@
+MainMenuBarArtFrame.LeftEndCap:Hide()
+MainMenuBarArtFrame.RightEndCap:Hide()
+MainMenuBarArtFrame.PageNumber:Hide()
+
+ActionBarUpButton:Hide()
+ActionBarDownButton:Hide()
+
+MainMenuBarArtFrameBackground:Hide()
